febe74f76963b805deda53c61064d076119b40fe
Matt Moncayo <matthew.moncayo@gmail.com>
2020-07-02 00:51:11 -0700

    updated models directory to correctly pass the right parameters for each type of query method

--- models/burger.js
+++ models/burger.js
@@ -1,18 +1,18 @@
-var orm = require("../config/orm");
+const orm = require("../config/orm.js");
 
-var burger = {
+const burger = {
     selectAll: function (cb) {
         orm.selectAll("burgers", function (res) {
             cb(res);
-        })
+        });
     },
-    createOne: function (cols, vals, cb) {
-        orm.createOne("burgers", cols, vals, function (res) {
+    createOne: function (vals, cb) {
+        orm.insertOne("burgers", "burger_name", vals, function (res) {
             cb(res);
         });
     },
-    updateOne: function (objColVals, condition, cb) {
-        orm.updateOne("burgers", objColVals, condition, function (res) {
+    updateOne: function (objColVals, cb) {
+        orm.updateOne("burgers", "devoured", objColVals, function (res) {
             cb(res);
         });
     }
