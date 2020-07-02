78504a91ec5dbe774059770dfb22feed025f2666
Matt Moncayo <matthew.moncayo@gmail.com>
2020-07-02 00:48:47 -0700

    updated variables and restructured routing formats for all routing methods

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,52 +1,47 @@
 // import express and the burger model (burger.js) to use its database functions
-var express = require("express");
-var burger = require("../models/burger.js");
-
-var router = express.Router();
+const express = require("express");
+const burger = require("../models/burger.js");
+const router = express.Router();
 
 // creates all our routes and set up logic within those routes where needed
 // route for selecting all entries
-router.get("/", function (req, res) {
-    burger.selectAll(function (data) {
-        var hbsObject = {
-            burgers: data
-        };
-        console.log(hbsObject);
-        res.render("index", hbsObject);
+
+router.get("/", (req, res) => {
+    burger.selectAll((data) => {
+        let burgers = data.map(({ burger_name, id, devoured }) => ({
+            burger_name: burger_name,
+            id: id,
+            devoured: devoured
+        }));
+
+        let burgerObject = { burgers: burgers };
+        console.log(burgerObject);
+        res.render("index", burgerObject);
     });
 });
 
 // route for creating a new entry
-router.post("/api/burgers", function (req, res) {
-    burger.createOne([
-        "burger_name", "devoured"
-    ], [
-        req.body.burger_name, req.body.devoured
-    ], function (result) {
-        res.json({ id: result.insertId });
+router.post("/api/new", (req, res) => {
+    let burgerName = req.body.burger_name;
+
+    burger.createOne(burgerName, (result) => {
+        res.json( { id: result.insertId } );
     });
 });
 
 // route for updating an entry
-router.put("/api/burgers/:id", function (req, res) {
-    var condition = "id = " + req.params.id;
-
-    console.log("condition", condition);
-
-    burger.updateOne(
-        {
-            devoured: req.body.devoured
-        },
-        condition,
-        function (result) {
-            if (result.changedRows === 0) {
-                // if no rows were changed, then the ID must not exist, so 404
-                return res.status(404).end();
-            }
+router.put("/api/burgers/:id", (req, res) => {
+    let devouredID = req.params.id;
+    console.log("condition: ", devouredID);
+    burger.updateOne(devouredID, (result) => {
+        if (result.affectedRows == 0) {
+            return res.status(404).end();
+        } else {
             res.status(200).end();
-
         }
-    );
+    })
+    console.log(devouredID, " has been devoured");
+
 });
 
 // export routes for server.js to use.
