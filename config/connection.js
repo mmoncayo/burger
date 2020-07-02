5130a80d04cfb38e779eee3deb3076ad52074dd6
Matt Moncayo <matthew.moncayo@gmail.com>
2020-07-01 22:34:02 -0700

    updated connection.js to include const variables

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
+// set up MySQL connection.
+const mysql = require("mysql");
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
@@ -9,7 +9,7 @@ var connection = mysql.createConnection({
   database: "burgers_db"
 });
 
-// Make connection.
+// make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -18,5 +18,5 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
+// export connection for our ORM to use.
 module.exports = connection;
