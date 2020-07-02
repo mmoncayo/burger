b28ca3aa84ee71c6ffc45df979847a909ad64e4d
Matt Moncayo <matthew.moncayo@gmail.com>
2020-07-02 00:54:15 -0700

    handled and corrected formatting for front end js

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,46 +1,44 @@
+const addBtn = $("#submitBtn");
+const devourBtn = $("#devourBtn");
+
 $(function () {
-
-    $(".create-form").on("submit", function (event) {
+    // adds a new burger when you click on submit
+    $(".addBurger").on("submit", (event) => {
         event.preventDefault();
+        let newBurger = {
+            burger_name: $("#burger").val().trim()
+        }
+        console.log(newBurger);
 
-        var newBurger = {
-            burger_name: $("#bur").val().trim(),
-            devoured: $("[burger_name=devoured]:checked").val()
-        };
-
-        // send the POST request.
-        $.ajax("/api/burgers", {
-            type: "POST",
-            data: newBurger
-        }).then(
-            function () {
-                console.log("new burger CREATED");
-                // re-load the page to get the updated list
-                location.reload();
-            }
-        );
+        // this ensures the new burger name isn't an empty string
+        if (newBurger.burger_name != "") {
+            // make a POST api request to add a new burger to the db
+            $.ajax("/api/new", {
+                type: "POST",
+                data: newBurger
+            })
+                .then(() => {
+                    console.log("New burger added!");
+                    location.reload();
+                })
+                .catch((err) => {
+                    console.log(err);
+                    alert("Error! Please check the code, burger couldn't be added to the list.");
+                })
+            // alert user if the entry to submit a burger name was null
+        } else { alert("Error! You must give the burger a name.") }
     });
 
-
-    $(".change-devour").on("click", function (event) {
-        var id = $(this).attr("data-id");
-        var newDevour = $(this).attr("data-newdevour");
-        console.log("sending id=" + id + " value=" + newDevour);
-
-        var newDevourState = {
-            devoured: newDevour
-        };
-
-        // send the PUT request.
-        $.ajax("/api/burgers/" + id, {
+    // 'devour' the burger and move it to devoured burgers list
+    $(".devourBtn").on("click", (event) => {
+        let clickedId = event.target.id;
+        $.ajax(`/api/burgers/${clickedId}`, {
             type: "PUT",
-            data: newDevourState
-        }).then(
-            function () {
-                console.log("burger has been: ", newDevour);
-                // re-load the page to get the updated list
+        })
+            .then(() => {
+                console.log("A burger was devoured!");
                 location.reload();
-            }
-        );
-    });
-});
+            })
+    })
+
+})
